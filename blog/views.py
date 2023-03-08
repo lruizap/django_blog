b060cb6ecd6821c01b50c0863d1f0989a5a7048f
lruizap <lruizap2502@gmail.com>
2023-03-08 14:56:25 +0100

    Actualización del proyecto

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView, DetailView  # new
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (
+    CreateView, UpdateView, DeleteView)  # new
+from django.urls import reverse_lazy  # new
 from .models import Post
 
 
@@ -10,4 +13,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
-    context_object_name = 'anything_you_want'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):  # new
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
